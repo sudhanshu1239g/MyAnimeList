11c82fec302326162ccff15a65f1621e816151d7
sudhanshu <gsam1239h@gmail.com>
2026-05-01 01:56:26 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,10 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 
-const PORT=process.env.PORT || 8000;
 connectToDB();
 module.exports = app;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`)
+})
 
