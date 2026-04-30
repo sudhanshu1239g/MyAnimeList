7a36ae61fbb02dba1b875e982380222d81785a84
sudhanshu <gsam1239h@gmail.com>
2026-05-01 01:45:56 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectToDB = require("./src/config/database")
 
 const PORT=process.env.PORT || 8000;
 connectToDB();
+module.exports = app;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const cors = require("cors")
 
+app.use(cors({
+    origin: "https://my-anime-list-seven.vercel.app", // Your Vite frontend URL
+    credentials: true                // Allow cookies/headers
+}));
+
 const animeRoutes = require("./routes/animeRoutes");
 const authRoutes=require("./routes/authRoutes");
 
 const app=express();
 
-app.use(cors({
-    origin: "https://my-anime-list-seven.vercel.app", // Your Vite frontend URL
-    credentials: true                // Allow cookies/headers
-}));
+
 app.use(express.json());
 app.use("/api/animeList", animeRoutes);
 app.use("/api/auth", authRoutes);
